7942e1195bd7489528ae277e4bc773fdcf01fc49
ljlopes <ljlopes@daitan.com>
2020-03-11 10:53:26 -0300

    Complements CRUD methods.

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const Product = mongoose.model('Product');
 
+mongoose.set('useFindAndModify', false);
+
 module.exports = {
     async index(req, res){
         const products = await Product.find();
@@ -9,9 +11,27 @@ module.exports = {
         return res.json(products);
     },
 
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
     async store(req, res){
         const product = await Product.create(req.body);
 
         return res.json(product);
     },
+
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+    }
 };
