b9fe029a099cdd14d726d23a75ba6deadf8866e5
ljlopes <ljlopes@daitan.com>
2020-03-11 11:43:44 -0300

    Configures products pagination.

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -6,7 +6,8 @@ mongoose.set('useFindAndModify', false);
 
 module.exports = {
     async index(req, res){
-        const products = await Product.find();
+        const { page = 1 } = req.query;
+        const products = await Product.paginate({}, {page, limit: 10});
 
         return res.json(products);
     },
